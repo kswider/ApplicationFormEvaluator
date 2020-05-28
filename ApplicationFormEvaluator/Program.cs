1f63c4fdb720edc079c88f04c2c9f73af76f71bc
Krzysiek <krzyztof.swider@motorolasolutions.com>
2020-05-29 00:38:25 +0200

    Added basic classes and model for identifying if application form should pass.

---
+++ ApplicationFormEvaluator/Program.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationFormEvaluatorML.Model;
+using Microsoft.ML;
+using System;
+
+namespace ApplicationFormEvaluator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput();
+            Console.WriteLine("Please provide your ECTS deficit application form");
+            input.Text = Console.ReadLine();
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+
+            if (result.Prediction)
+                Console.WriteLine("Your application form passed!");
+            else
+                Console.WriteLine("Your application form did not pass. Your ECTS deficit is too high :(");
+        }
+    }
+}
